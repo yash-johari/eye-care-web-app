612e1ade058892ae3185f11c0c9fea2d3e65d202
unknown <yashjohari2508@gmail.com>
2025-03-22 18:20:22 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Footer() {
-  return <footer className="footer-text">Made with ❤️ by Yash Johari</footer>;
+  return (
+    <footer className="footer-text">
+      © 2025 Eye Care Web App
+    </footer>
+  );
 }
 
 export default Footer;
