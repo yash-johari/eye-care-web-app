e6590e9630cdbf08361f9b3161ef81dbd11d781d
unknown <yashjohari2508@gmail.com>
2025-04-29 21:07:44 +0530

    change API URL in doctors.js

--- frontend/src/doctors.js
+++ frontend/src/doctors.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchDoctors = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/doctors');
+      const response = await axios.get('https://eyecareapp-backend.onrender.com//doctors');
       return response.data;
     } catch (error) {
       console.error("Error fetching doctors data: ", error);
